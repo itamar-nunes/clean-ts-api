0ebac29ce8f376989eec465d0ed32c3e52217c7d
Itamar Nunes <itamarbel@gmail.com>
2020-05-16 14:44:19 -0300

    test: git eslint test

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
-        statusCode: 40000,
+        statusCode: 4000,
         body: new Error('Missign param: name')
       }
     }
