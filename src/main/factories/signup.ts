0baab8a89a2815a08f21af367b23b8aa9fed5b2f
Itamar Nunes <itamarbel@gmail.com>
2020-05-20 17:48:37 -0300

    feat: new class LogControllerDecorator and LogMongoRepository creates an error log in mongodb

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,13 +3,16 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignupController = (): SignUpController => {
+export const makeSignupController = (): Controller => {
   const salt = 12
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return signUpController
+  const logControllerDecorator = new LogControllerDecorator(signUpController)
+  return logControllerDecorator
 }
