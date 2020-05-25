d9016ce17cafa02ebbaf0436752a6efa35cf571f
Itamar Nunes <itamarbel@gmail.com>
2020-05-25 11:44:25 -0300

    refactor: move data layer interfaces to folders

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,5 +1,5 @@
 import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
-import { AddAccountRepository } from '../../protocols/add-account-repository'
+import { AddAccountRepository } from '../../protocols/db/add-account-repository'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
