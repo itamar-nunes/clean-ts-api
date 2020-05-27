5f3f2c1bc045ba87bad55bbd18caf82315ba440e
Itamar Nunes <itamarbel@gmail.com>
2020-05-27 14:34:29 -0300

    chore: add DockerCompose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:12
 WORKDIR /usr/src/clean-node-api
 COPY ./package.json .
 RUN nmp install --only=prod
-COPY ./dist ./dist
-EXPOSE 5000
-RUN npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,8 @@
 export default {
+  // Quando usar o Docker. Devemos apontar para o "mongo" DNS, e não para localhost.
+  // pois o Docker gera o endereço com o mesmo nome do "services" do Docker-compose.yml
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/clean-node-api',
+
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5T'
