69e72447583afa1b6002d27f352ef61680cc7362
Itamar Nunes <itamarbel@gmail.com>
2020-05-28 16:44:16 -0300

    feat: make DbAddAccount calls LoadAccountByEmailRepository with email

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,17 +1,17 @@
-import { AddAccount, AddAccountModel, AccountModel, Hasher } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Hasher, LoadAccountByEmailRepository } from './db-add-account-protocols'
 import { AddAccountRepository } from '../../protocols/db/account/add-account-repository'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
     const hashedPassword = await this.hasher.hash(accountData.password)
-
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-
     return account
   }
 }
