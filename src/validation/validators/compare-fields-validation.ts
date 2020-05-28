01d5b03c3a8946d81b2b3f4e22ed106260c408f2
Itamar Nunes <itamarbel@gmail.com>
2020-05-28 17:54:27 -0300

    refactor: move validation to its onw layer

--- src/validation/validators/compare-fields-validation.ts
+++ src/validation/validators/compare-fields-validation.ts
@@ -1,5 +1,5 @@
-import { Validation } from '../../protocols/validations'
-import { InvalidParamError } from '../../errors'
+import { Validation } from '../../presentation/protocols'
+import { InvalidParamError } from '../../presentation/errors'
 
 export class CompareFieldsValidation implements Validation {
   constructor (
