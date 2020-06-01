05eb715818a643f8dcebbb539bddcea8c95397d9
Itamar Nunes <itamarbel@gmail.com>
2020-06-01 15:59:20 -0300

    refactor: move files to folders

--- src/main/factories/controllers/login/login/login-controller-factory.ts
+++ src/main/factories/controllers/login/login/login-controller-factory.ts
@@ -1,6 +1,6 @@
 import { LoginController } from '../../../../../presentation/controllers/login/login/login-controller'
 import { Controller } from '../../../../../presentation/protocols'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/db-authentication-factory'
 import { makeLoginValidation } from './login-validation-factory'
 import { makeLogDecoratorController } from '../../decorators/log-decorator-controller-factory'
 
--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '../../../../../presentation/controllers/login/signup/signup-controller'
 import { Controller } from '../../../../../presentation/protocols'
 import { makeSignupValidation } from './signup-validation-factory'
-import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeDbAuthentication } from '../../../usecases/account/db-authentication-factory'
 import { makeAddAccount } from '../../../usecases/account/db-add-account-factory'
 import { makeLogDecoratorController } from '../../decorators/log-decorator-controller-factory'
 
