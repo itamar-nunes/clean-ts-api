dd3afc0c6281b40dd47736db66d9bc76d7627dd2
Itamar Nunes <itamarbel@gmail.com>
2020-06-01 15:51:52 -0300

    refactor: refactor folders

---
+++ src/main/factories/controllers/add-survey/login/signup/signup-validation-factory.ts
@@ -0,0 +1,16 @@
+import { ValidationComposite, RequiredFieldValidation, CompareFieldsValidation, EmailValidation } from '../../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols/validations'
+import { EmailValidatorAdapter } from '../../../../../infra/validators/email-validator-adapter'
+
+export const makeSignupValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+
+  validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
+
+  return new ValidationComposite(validations)
+}
---
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -0,0 +1,11 @@
+import { SignUpController } from '../../../../../presentation/controllers/login/signup/signup-controller'
+import { Controller } from '../../../../../presentation/protocols'
+import { makeSignupValidation } from './signup-validation-factory'
+import { makeDbAuthentication } from '../../../usecases/authentication/db-authentication-factory'
+import { makeAddAccount } from '../../../usecases/add-account/db-add-account-factory'
+import { makeLogDecoratorController } from '../../decorators/log-decorator-controller-factory'
+
+export const makeSignupController = (): Controller => {
+  const controller = new SignUpController(makeAddAccount(), makeSignupValidation(), makeDbAuthentication())
+  return makeLogDecoratorController(controller)
+}
