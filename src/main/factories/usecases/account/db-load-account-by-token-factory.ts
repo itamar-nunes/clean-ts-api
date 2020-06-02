9a217870d37ea4e59ad373e6161e2e02c31063bb
Itamar Nunes <itamarbel@gmail.com>
2020-06-02 16:36:43 -0300

    chore: move DbLoadAccountByToken to account folder

--- src/main/factories/usecases/account/db-load-account-by-token-factory.ts
+++ src/main/factories/usecases/account/db-load-account-by-token-factory.ts
@@ -1,6 +1,6 @@
 import env from '../../../config/env'
 import { LoadAccountByToken } from '../../../../domain/usecases/load-account-by-token'
-import { DbLoadAccountByToken } from '../../../../data/usecases/load-account-by-token/db-load-account-by-token'
+import { DbLoadAccountByToken } from '../../../../data/usecases/account/db-load-account-by-token'
 import { JwtAdapter } from '../../../../infra/cryptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../../infra/db/mongodb/account/account-mongo-repository'
 
