febc1de1009014760a9559d6d9fbcb169451c5bf
Itamar Nunes <itamarbel@gmail.com>
2020-06-02 15:12:03 -0300

    chore: mode add-survey-controller to survey folder

--- src/main/factories/controllers/survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey-controller-factory.ts
@@ -1,5 +1,5 @@
 import { Controller } from '../../../../presentation/protocols'
-import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey/add-survey-controller'
+import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey-controller'
 import { makeLogDecoratorController } from '../decorators/log-decorator-controller-factory'
 import { makeAddSurveyValidation } from './add-survey-validation'
 import { makeDbAddSurvey } from '../../usecases/survey/db-add-survey-factory'
