2281a3bd52105c89b51875061b0b8aee68881fab
Itamar Nunes <itamarbel@gmail.com>
2020-06-05 15:36:06 -0300

    refactor: change models to use Omit<>

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,15 @@
 import { AccountModel } from '@/domain/models/account'
 
-export type AddAccountModel = {
-  name: string
-  email: string
-  password: string
-}
+export type AddAccountModel = Omit<AccountModel, 'id'>
 
 export interface AddAccount {
   add (account: AddAccountModel): Promise<AccountModel>
 }
+
+// Modo tradicional de criar uma "interface" usando o "type" e definindo todos os campos, exceto os que você não quer.
+// Ver tambem acima, o modo usando o utilitátio do TS chamado "Omit<>"
+// export type AddAccountModel = {
+//   name: string
+//   email: string
+//   password: string
+// }
--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,11 +1,15 @@
-import { SurveyAnswerModel } from '@/domain/models/survey'
+import { SurveyModel } from '@/domain/models/survey'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
 
 export interface AddSurvey {
   add (data: AddSurveyModel): Promise<void>
 }
+
+// Modo tradicional de criar uma "interface" usando o "type" e definindo todos os campos, exceto os que você não quer.
+// Ver tambem acima, o modo usando o utilitátio do TS chamado "Omit<>"
+// export type AddSurveyModel = {
+//   question: string
+//   answers: SurveyAnswerModel[]
+//   date: Date
+// }
