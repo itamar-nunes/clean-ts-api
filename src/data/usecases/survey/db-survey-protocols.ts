859222b7230888cf60523e6f7cd60a09faa927aa
Itamar Nunes <itamarbel@gmail.com>
2020-06-08 15:42:57 -0300

    refactor: new file db-survey-protocols to use imports

---
+++ src/data/usecases/survey/db-survey-protocols.ts
@@ -0,0 +1,11 @@
+export * from '@/domain/models/survey'
+
+export * from '@/domain/usecases/add-survey'
+export * from '@/data/protocols/db/survey/add-survey-repository'
+
+export * from '@/domain/usecases/load-surveys'
+export * from '@/data/protocols/db/survey/load-surveys-repository'
+
+export * from '@/domain/usecases/load-surveys-by-id'
+export * from '@/domain/models/survey'
+export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
