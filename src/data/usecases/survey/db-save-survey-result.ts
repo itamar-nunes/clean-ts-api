9d4e19c1834d22cea3fbe842d687e6cb65c08cc0
Itamar Nunes <itamarbel@gmail.com>
2020-06-08 16:07:58 -0300

    feat: make DbSaveSurveyResult calls SaveSurveyResultRepository with corretc values

---
+++ src/data/usecases/survey/db-save-survey-result.ts
@@ -0,0 +1,10 @@
+import { SaveSurveyResult, SaveSurveyResultModel, SurveyResultModel, SaveSurveyResultRepository } from './db-survey-protocols'
+
+export class DbSaveSurveyResult implements SaveSurveyResult {
+  constructor (private readonly saveSurveyResultRepository: SaveSurveyResultRepository) {}
+
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResult = await this.saveSurveyResultRepository.save(data)
+    return surveyResult
+  }
+}
--- src/data/usecases/survey/db-survey-protocols.ts
+++ src/data/usecases/survey/db-survey-protocols.ts
@@ -8,3 +8,7 @@ export * from '@/data/protocols/db/survey/load-surveys-repository'
 
 export * from '@/domain/usecases/load-surveys-by-id'
 export * from '@/data/protocols/db/survey/load-survey-by-id-repository'
+
+export * from '@/domain/usecases/save-survey-result'
+export * from '@/domain/models/survey-result'
+export * from '@/data/protocols/db/survey/save-survey-result-repository'
