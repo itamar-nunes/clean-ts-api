c5741eaf16a0c76cb927ed9d90eb6f67ec9e5d23
Itamar Nunes <itamarbel@gmail.com>
2020-07-10 21:47:54 -0300

    refactor: add folder test to implements test examples

--- src/data/protocols/test/index.ts
+++ src/data/protocols/test/index.ts
@@ -1,2 +1,3 @@
 // import and export your protocols tests here
 // Ex: export * from './mock-cryptography'
+// Ex: export * from './mock-db-account'
---
+++ src/data/protocols/test/mock-db-account.ts
@@ -0,0 +1,1 @@
+// Implements and export here your mocks, from db/account folder, to use in your tests
