37ed9cefa71898890ed1eecbad00b42c89176a01
Itamar Nunes <itamarbel@gmail.com>
2020-07-14 17:42:35 -0300

    refactor: add mock-email-validator and mock-email-validation in test folder

---
+++ src/validation/test/index.ts
@@ -0,0 +1,4 @@
+// import and export your protocols tests here
+// Ex:
+// export * from './mock-email-validator'
+// export * from './mock-validation'
---
+++ src/validation/test/mock-validation.ts
@@ -0,0 +1,1 @@
+// Implements and export here your mocks, from validators folder/, to use in your tests
